ca40b7e8dd12c02285cbef480a0d2e4846cd5a28
Pozzame <pozzame@gmail.com>
2024-07-24 15:30:11 +0200

    Ok

--- Program.cs
+++ Program.cs
@@ -16,23 +16,11 @@ Random rng = new Random();
 int segreto = rng.Next(0,5);
 if ((segreto + num) %2 == 0 )
 {
-    if (pod == "p")
-    {
-        Console.WriteLine($"Hai vinto! :-D, il numero scelto era {segreto}");
-    }
-    else
-    {
-        Console.WriteLine($"Hai perso! :-(, il numero scelto era {segreto}");
-    }
+    if (pod == "p") Console.WriteLine($"Hai vinto! :-D, il numero scelto era {segreto}");
+    else Console.WriteLine($"Hai perso! :-(, il numero scelto era {segreto}");
 }
 else if ((segreto + num) %2 != 0)
 {
-    if (pod == "d")
-    {
-        Console.WriteLine($"Hai vinto! :-D, il numero scelto era {segreto}");
-    }
-    else
-    {
-        Console.WriteLine($"Hai perso! :-(, il numero scelto era {segreto}");
-    }
+    if (pod == "d") Console.WriteLine($"Hai vinto! :-D, il numero scelto era {segreto}");
+    else Console.WriteLine($"Hai perso! :-(, il numero scelto era {segreto}");
 }
