c8e78fb8522f80394e835274ea7ae325c8e42fc3
Pozzame <pozzame@gmail.com>
2024-07-24 14:41:06 +0200

    Ok

--- Program.cs
+++ Program.cs
@@ -1,29 +1,16 @@
-﻿/*start: Console.WriteLine("Pari o dispari? (p/d)");
-string pod = Console.ReadLine()!.ToLower();*/
-string pod = "a";
+﻿string pod = "a";
 while (pod != "p" && pod != "d")
 {
     Console.WriteLine("Pari o dispari? (p/d)");
     pod = Console.ReadLine()!.ToLower();
 }
-/*if (pod != "p" && pod != "d")
-{
-    Console.WriteLine("Scelta non valida");
-    goto start;
-}*/
+
 int num = 6;
 while (num < 0 || num > 5)
 {
     Console.WriteLine("Scegli un numero tra 0 e 5.");
     num = Convert.ToInt32(Console.ReadLine());
 }
-/*num: Console.WriteLine("Scegli un numero tra 0 e 5.");
-int num = Convert.ToInt32(Console.ReadLine());
-if (num < 0 || num > 5)
-{
-    Console.WriteLine("Scelta non valida");
-    goto num;
-}*/
 
 Random rng = new Random();
 int segreto = rng.Next(0,5);
